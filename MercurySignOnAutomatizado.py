9b425cea94d2a9a87e30d58777c5ccafb106e253
Wilber Bertel Toscano <ing,wilberbertel@gmail.com>
2024-05-21 20:25:05 -0500

    Automatizacion Con Sign on Con variables

--- MercurySignOnAutomatizado.py
+++ MercurySignOnAutomatizado.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
+Url = "https://demo.guru99.com/test/newtours/index.php"
+User = "WB@gmail.com"
+password ="Prueba.01"
 driver = webdriver.Chrome()
 driver.get("https://demo.guru99.com/test/newtours/index.php")
 driver.maximize_window()
@@ -14,42 +16,32 @@ buttonRegister = driver.find_element(By.LINK_TEXT, "SIGN-ON") .click()
 time.sleep(2)
  
 try:
-    registerImag = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//img[@src='images/mast_signon.gif']"))
-        )
-    ad_close_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Close')]"))
-        ).ad_close_button.click()
-    if registerImag:
-        print("!Bienvenido a la página del registro¡")
-        print("Por favor procede a llenar los campos")
+    SignOnImag = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//img[@src='images/hdr_findflight.gif']"))
+            )    
+    if SignOnImag:
+            print("!Bienvenido al modulo de Inicio de sesion¡")
+            print("Por favor procede a llenar los campos")
 
-    userName = driver.find_element(By.XPATH, "//input[contains(@name,'userName')]") 
-    userName.send_keys("WB@gmail.com")
+    userName = driver.find_element(By.XPATH, "//input[contains(@name,'userName')]") .send_keys(User)
     time.sleep(1)
 
-    password = driver.find_element(By.XPATH, "//input[contains(@name,'password')]") 
-    password.send_keys("Prueba.01")
+    driver.find_element(By.XPATH, "//input[contains(@name,'password')]").send_keys(password)
     time.sleep(1)
 
     button = driver.find_element(By.XPATH, "//input[contains(@type,'submit')]") .click()
     time.sleep(2)
 
-    confirmationMessage = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//b[contains(.,'Thank you for Loggin.')]"))
-        )   
-    
-    if ad_close_button:
-        print("publicidad encontrada")
-        print(ad_close_button.text)
-except Exception as e:
-    print("publicidad no encontrada", e)
+    confirmation_message = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//b[contains(.,'Thank you for Loggin.')]"))
+        )
 
-    if confirmationMessage:
-        print("Mensaje de confirmación encontrado")
-        print(confirmationMessage.text)
+    if confirmation_message:
+            print("Mensaje de confirmación encontrado")
+            print(confirmation_message.text)
+
 except Exception as e:
-    print("Error al cargar la pagina de SIGN-ON", e)
-    
+        print("Error al cargar la pagina de Sign on", e)
+
 finally:
-    driver.close()
+        driver.quit()
